462f3a70e684faf7994a61e49a14460c9e4791bc
Dev Pathak <114007535+devpathak0212@users.noreply.github.com>
2026-08-17 16:53:50 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS for React
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://searchwikipedia.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
